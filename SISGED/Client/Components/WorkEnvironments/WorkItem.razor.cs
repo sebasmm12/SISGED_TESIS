fe0ac033955f45b0e3b4cce3fc5ed4ba3ff0447e
Sebastian Miranda <sebastian.miranda@coalfire.com>
2025-09-02 20:26:50 -0500

    Add document title display and refactor WorkItem logic
    Updated `WorkItem.razor` to conditionally display the document title based on `Item.Client`. Introduced `GetDocumentTitle` method for title retrieval from `DossierTrayResponse`. Refactored `WorkItem.razor.cs` to consolidate accepted statuses and document types, removing redundant definitions. Added necessary using directives and included `System.Text.Json.Serialization` in `DocumentContentDTO.cs` for serialization support.

--- SISGED/Client/Components/WorkEnvironments/WorkItem.razor.cs
+++ SISGED/Client/Components/WorkEnvironments/WorkItem.razor.cs
@@ -1,43 +1,57 @@
+using System.Text.Json;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using SISGED.Client.Helpers;
+using SISGED.Shared.DTOs;
+using SISGED.Shared.Models.Responses.DossierTray;
 using SISGED.Shared.Models.Responses.PublicDeed;
 
-namespace SISGED.Client.Components.WorkEnvironments
+namespace SISGED.Client.Components.WorkEnvironments;
+
+public partial class WorkItem
 {
-    public partial class WorkItem
+    private readonly IEnumerable<string> accepetedStatusToDocument = new List<string>
     {
-        [Parameter]
-        public Item Item { get; set; } = default!;
-        [Parameter]
-        public PublicDeedFilterResponse PublicDeed { get; set; } = default!;
-        
-        private readonly IEnumerable<string> accepetedStatusToDocument = new List<string>()
-        {
-            "registered",
-            "evaluated"
-        };
+        "registered",
+        "evaluated"
+    };
 
-        private readonly IEnumerable<string> acceptedStatusToPublicDeed = new List<string>()
-        {
-            "choosen"
-        };
+    private readonly IEnumerable<string> acceptedDocumentTypes = new List<string>
+    {
+        "OficioDesignacionNotario",
+        "ConclusionFirma",
+        "OficioBPN",
+        "ResultadoBPN"
+    };
 
-        private readonly IEnumerable<string> acceptedStatusToInfo = new List<string>()
-        {
-            "inspace",
-            "registered",
-            "evaluated",
-            "choosen"
-        };
+    private readonly IEnumerable<string> acceptedStatusToInfo = new List<string>
+    {
+        "inspace",
+        "registered",
+        "evaluated",
+        "choosen"
+    };
 
-        private readonly IEnumerable<string> acceptedDocumentTypes = new List<string>()
-        {
-            "OficioDesignacionNotario",
-            "ConclusionFirma",
-            "OficioBPN",
-            "ResultadoBPN"
-        };
+    private readonly IEnumerable<string> acceptedStatusToPublicDeed = new List<string>
+    {
+        "choosen"
+    };
 
-        private string? ToolColor => Item.OriginPlace == "tools" ? "color-tool": null;
+    [Parameter] public Item Item { get; set; } = default!;
+
+    [Parameter] public PublicDeedFilterResponse PublicDeed { get; set; } = default!;
+
+    private string? ToolColor => Item.OriginPlace == "tools" ? "color-tool" : null;
+
+
+    private string GetDocumentTitle(DossierTrayResponse dossierTray)
+    {
+        var currentDocumentTitle =
+            JsonSerializer.Deserialize<DocumentContentDTO>(JsonSerializer.Serialize(dossierTray.Document!.Content), new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+
+        return currentDocumentTitle!.Title;
     }
 }
--- SISGED/Shared/DTOs/DocumentContentDTO.cs
+++ SISGED/Shared/DTOs/DocumentContentDTO.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
+﻿using System.Text.Json.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace SISGED.Shared.DTOs
 {
